58addd17fefbf35188deefe4f8f9acd9a2f04162
Victor Guadarrama <victor.guadarrama@infosel.com>
2025-01-29 23:37:52 -0600

    Add validPalindrome

---
+++ validPalindrome.java
@@ -0,0 +1,26 @@
+public class validPalindrome {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+}
